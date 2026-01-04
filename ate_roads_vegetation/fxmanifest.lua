c96ed11d0ad654529a9c86374ab61a05d8d0015a
Atenea Store <106404351+atenea03@users.noreply.github.com>
2026-01-04 15:59:52 +0100

    Update fxmanifest version and comment out files
    Updated version from 1.0.1 to 1.0.2 and commented out file declarations.

--- ate_roads_vegetation/fxmanifest.lua
+++ ate_roads_vegetation/fxmanifest.lua
@@ -4,15 +4,16 @@ lua54 'yes'
 this_is_a_map 'yes'
 author 'Atenea'
 description 'Map Road Vegetation'
-version '1.0.1'
+version '1.0.2'
 
 ---------------------------------------------------------------------------
 -- INCLUDED FILES
 ---------------------------------------------------------------------------
-files {
-    'stream/**/*.ytyp',
-}
+--files {
+    --'stream/**/*.ytyp',
+--}
 
-data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
 dependency '/assetpacks'
+
 
